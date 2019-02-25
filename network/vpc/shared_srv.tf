7f40ab2fcc0609e3109446ff9e783058b343c931
Matt Kimberley <matt.kimberley@hotmail.com>
2019-02-25 20:14:12 +0000

    testing vpc

--- network/vpc/shared_srv.tf
+++ network/vpc/shared_srv.tf
@@ -1,1 +1,16 @@
- 
+module "vpc" {
+  name = "${var.vpc_name}"
+  cidr = "${var.vpc_cidr}"
+
+  azs             = "${var.azs}"
+  private_subnets = "${var.private_subnets}"
+  public_subnets  = "${var.public_subnets}"
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "${var.environment_name}"
+  }
+}
