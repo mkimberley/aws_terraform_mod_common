4f0e490291f20ade23af54c01e66211933261829
Matt Kimberley <matt.kimberley@hotmail.com>
2019-09-05 21:13:45 +0100

    work in progress

---
+++ remotestate-map/outputs.tf
@@ -0,0 +1,9 @@
+output "frontend_subnet" {
+    value ="${local.frontend_subnet}"
+}
+output "backend_subnet" {
+    value ="${local.backend_subnet}"
+}
+output "allzone_subnet" {
+    value ="${local.allzone_subnet}"
+}
