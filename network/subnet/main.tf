6842ef3a9cb6ad00f04d54872e6d8f8edc6f9b18
Matt Kimberley <matt.kimberley@hotmail.com>
2019-09-20 16:37:34 +0100

    wip

--- network/subnet/main.tf
+++ network/subnet/main.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "this" {
   vpc_id                        = "${var.sub_vpc_id}"
-  cidr_block                    = "${var.vpc_primary_subnet}"
+  cidr_block                    = "${var.sub_cidr_block}"
 
 }
