0d031a86334959f89c9c85852e0603626c1a2c63
Matt Kimberley <matt.kimberley@hotmail.com>
2019-09-23 21:01:03 +0100

    wip

--- remotestate-map/vpc-details.tf
+++ remotestate-map/vpc-details.tf
@@ -1,6 +1,6 @@
-Locals {
-    vpc_primary_subnet_map  = "${zipmap(data.terraform_remote_state.vpc.primary_cidr_subnets_vpc_details)}"
-    frontend_subnet         = "${data.terraform_remote_state.vpc.prim}"
+locals {
+    vpc_primary_subnet_map  = "${data.terraform_remote_state.generic_vpc.primary_subnets_map}"
+    frontend_subnet         = "${data.terraform_remote_state.vpc.primary_subnets_map}"
     backend_subnet          = "${data.terraform_remote_state.backend_subnet_id}"
     allzone_subnet          = "${merge(frontend_subnet, backend_subnet)}"
 }
