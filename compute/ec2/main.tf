33be6b5eaffd4b879c14a3a19ef0711374c50ce8
Matt Kimberley <matt.kimberley@hotmail.com>
2019-11-06 20:00:51 +0000

    work in progress

--- compute/ec2/main.tf
+++ compute/ec2/main.tf
@@ -26,13 +26,13 @@ module "tag" {
 }
 resource "aws_network_interface" "network_interface" {
   #subnet_id            = "${lookup(data.terraform_remote_state.*.vpc.primary_subnets_map, "${var.ec2_subnet_name}")}"
-  subnet_id            = "${flatten(element(data.terraform_remote_state.vpc.outputs.private_subnets, 0))}"
+  subnet_id            = "${data.terraform_remote_state.vpc.outputs.private_subnets[0]}"
   security_groups      = "${var.security_group_data}"
 }
 
 resource "aws_network_interface_attachment" "network_interface_attachment" {
-  instance_id          = "${aws_instance.this.id}"
-  network_interface_id = "${aws_network_interface.network_interface.id}"
+ instance_id          = "${aws_instance.this.id}"
+ network_interface_id = "${aws_network_interface.network_interface.id}"
   device_index         = 0
 }
 
@@ -45,6 +45,6 @@ module "ami_search" {
 resource "aws_instance" "this" {
   ami                 = "${module.ami_search.ami_id}"
   instance_type       = "${var.instance_type}"
-
+  
   tags = "${module.tag.all_tags}"
-} 
+}
--- examples/example_ec2/backend.tf
+++ examples/example_ec2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mkimberley84tfstate"
-    key    = "production/core/terraform.tfstate"
+    key    = "production/ec2_example/terraform.tfstate"
     region = "eu-west-2"
   }
 }
