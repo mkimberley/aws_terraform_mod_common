f683aea6bf3dbf1abf631615091e30c0124d117d
Matt Kimberley <matt.kimberley@hotmail.com>
2020-01-02 18:05:31 +0000

    Public IP on Launch

--- network/subnet/main.tf
+++ network/subnet/main.tf
@@ -8,7 +8,9 @@ locals {
 resource "aws_subnet" "this" {
   vpc_id                        = "${var.sub_vpc_id}"
   cidr_block                    = "${var.sub_cidr_block}"
+  map_public_ip_on_launch       = "${var.sub_public_ip_on_launch}"
 
+  
   tags = "${merge(
       local.tags,"${var.sub_tags}"
   )}"
